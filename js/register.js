c95e724dcb60392c3832f3bdd6d92bcda2f5f6b1
Abdessattar Louhichi <louhichi.abdessattar@yahoo.com>
2022-10-06 14:20:37 +0100

    création du projet

---
+++ js/register.js
@@ -0,0 +1,11 @@
+var name = document.getElementById('name');
+var lastName = document.getElementById('lastName');
+var Email = document.getElementById('inputEmail');
+function register(e) {
+    e.preventDefault();
+
+}
+
+
+var button = document.getElementById('signUP');
+button.addEventListener('click',register)
