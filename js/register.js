c462c7ac6535bfad034c924d0300dc7fffabd6fc
Abdessattar Louhichi <louhichi.abdessattar@yahoo.com>
2022-10-06 23:02:27 +0100

    script password validation

--- js/register.js
+++ js/register.js
@@ -3,6 +3,7 @@ var lastName = document.getElementById('lastName');
 var Email = document.getElementById('inputEmail');
 var password = document.getElementById('password');
 var confirmPw = document.getElementById('confirmPw');
+var errorEmail = document.getElementById('errorEmail');
 var validForm =true;
 function register(e) {
     e.preventDefault();
@@ -25,20 +26,34 @@ function register(e) {
         lastName.classList.add('is-valid');
     }
     //Validation Email
+    console.log(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(Email.value));
     if (Email.value =="") {
         Email.classList.add('is-invalid');
         Email.classList.remove('is-valid');
+        errorEmail.innerHTML = 'Email obligatoire'
         validForm = false; 
-    } else{
+    } else if(Email.value !="" && ! /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(Email.value)){
+        Email.classList.add('is-invalid');
+        Email.classList.remove('is-valid');
+        errorEmail.innerHTML = 'Invalid Email'
+        validForm = false;
+    }else{
         Email.classList.remove('is-invalid');
         Email.classList.add('is-valid');
     }
     //Validation password
+    console.log(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/.test(password.value));
     if (password.value =="") {
         password.classList.add('is-invalid');
         password.classList.remove('is-valid');
+        ErrorPw.innerHTML ='Mot de passe obligatoire'
         validForm = false; 
-    } else{
+    } else if(password.value !="" && ! /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/.test(password.value)){
+        password.classList.add('is-invalid');
+        password.classList.remove('is-valid');
+        ErrorPw.innerHTML =' Passwords must be at least 8 characters && Include at least: 1 lowercase letter, 1 capital letter, 1 number, 1 special character'
+        validForm = false;
+    } else {
         password.classList.remove('is-invalid');
         password.classList.add('is-valid');
     }
